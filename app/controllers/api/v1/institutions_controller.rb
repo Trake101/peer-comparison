b96ce78319df6879c23895dd913bc631db4a9a4b
David Peterson <pete2786@umn.edu>
2014-02-19 11:12:51 -0600

    Made text input the default for selecting a school.

--- app/controllers/api/v1/institutions_controller.rb
+++ app/controllers/api/v1/institutions_controller.rb
@@ -7,7 +7,7 @@ module Api
 		  def index
 		    @institutions = Institution.all(:order => :inst_name)
 
-		    respond_with @institutions, only: [:id, :inst_name, :unit_id, :city, :state], status: 200
+		    respond_with @institutions, only: [:id, :inst_name, :unit_id, :city, :state, :sector], status: 200
 		  end
 
 		end
